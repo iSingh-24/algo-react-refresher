a79e786a31e95c759494d7cdf2fe3af909ba8554
Inderprit Singh <singhinderprit1992@gmail.com>
2025-05-31 19:02:34 -0400

    created user form as a class component

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import User from "./Components/User/User";
+import CreateUserForm from "./Components/User/CreateUserForm";
 
 export const App = () => {
   return (
     <div>
       <User />
+      <CreateUserForm />
     </div>
   );
 };
---
+++ client/Components/User/CreateUserForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// const CreateUserForm = () =>{
+
+// }
+
+class CreateUserForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Create User Form Component</div>;
+  }
+}
+
+export default CreateUserForm;
