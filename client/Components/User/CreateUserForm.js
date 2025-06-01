63c8f3a056143e53aa36ebb85c3cb68e1e31a567
Inderprit Singh <singhinderprit1992@gmail.com>
2025-06-01 12:23:42 -0400

    set up functional component as an alternative for CreateUserForm component

--- client/Components/User/CreateUserForm.js
+++ client/Components/User/CreateUserForm.js
@@ -1,22 +1,37 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class CreateUserForm extends Component {
-  constructor(props) {
-    super(props);
+// class CreateUserForm extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      firstName: "Kobe",
-      lastName: "Bryant",
-    };
-  }
+//     this.state = {
+//       firstName: "Kobe",
+//       lastName: "Bryant",
+//     };
+//   }
 
-  PrintUserName = () => {
-    return `${this.state.firstName} ${this.state.lastName}`;
-  };
+//   PrintUserName = () => {
+//     return `${this.state.firstName} ${this.state.lastName}`;
+//   };
 
-  render() {
-    return <div>{`Hello my name is ${this.PrintUserName()}`}</div>;
-  }
-}
+//   render() {
+//     return <div>{`Hello my name is ${this.PrintUserName()}`}</div>;
+//   }
+// }
+
+// export default CreateUserForm;
+
+/**FUNCTIONAL COMPONENT */
+
+import React, { useState } from "react";
+
+const CreateUserForm = () => {
+  const [user, setUser] = useState({ firstName: "Kobe", lastName: "Bryant" });
+  const { firstName, lastName } = user;
+
+  console.log(user, "here is current user");
+
+  return <div>{`The current user is ${firstName} ${lastName}`}</div>;
+};
 
 export default CreateUserForm;
